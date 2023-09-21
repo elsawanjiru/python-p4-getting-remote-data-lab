70db08d370ae6477cf3090928f8d1b4507055193
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-09-21 12:53:50 +0300

    Completed code challenge

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,22 @@
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            response_body = response.content.decode('utf-8')
+            print("Response content:")
+            print(response_body)
+            return response_body
+        else:
+            raise Exception(f"Request failed with status code: {response.status_code}")
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            return json.loads(response_body)
+        except json.JSONDecodeError as e:
+            raise Exception(f"Failed to parse JSON: {str(e)}")
